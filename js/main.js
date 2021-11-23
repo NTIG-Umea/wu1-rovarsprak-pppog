4a76ac4a58b553a74cce44a0cbc53ee0de3d72d1
pppog <77719393+pppog@users.noreply.github.com>
2021-11-23 08:16:50 +0100

    did stuff

--- js/main.js
+++ js/main.js
@@ -19,10 +19,41 @@ document.addEventListener('DOMContentLoaded', () => {
         display(translation, message);
     });
 
+    textbox.addEventListener('change', (e) => {
+        // läs in texten från textboxen
+        const text = textbox.value;
+        // skicka texten till funktionen för att översätta
+        const translation = translate(text);
+        // Hur kan du rensa textboxen?
+        // skriv ut översättningen i message
+        display(translation, message);
+    });
+
 });
 
 const translate = (text) => {
     // översätt texten till rövarspråket
+    let nyttOrd = "";
+    for(var i = 0; i < text.length; i++)
+    {
+        let k = text.charAt(i);
+        if(k == 'a' || k == 'o' || k == 'u' || k == 'i' || k == 'e' || k == 'ä' || k == 'å' || k == 'ö' || k == '')
+        {
+            nyttOrd = nyttOrd + k;
+        }
+        else
+        {
+            if(i == 0)
+            {
+                nyttOrd = k + "o" + k.toLowerCase();
+            }
+            else
+            {
+            nyttOrd = nyttOrd + k.toLowerCase() + "o" + k.toLowerCase();
+            }
+        }
+    }
+    text = nyttOrd;
     console.log(text)
     return text;
 };
